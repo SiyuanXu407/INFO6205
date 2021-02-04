f6272a5d3619e9ef68e12fa9df85da400256fbc8
SiyuanXu407 <siyuanxu@email.arizona.edu>
2021-02-04 09:12:00 -0700

    1

--- src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
@@ -10,63 +10,74 @@ import edu.neu.coe.info6205.util.Config;
 
 public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
 
-    /**
-     * Constructor for any sub-classes to use.
-     *
-     * @param description the description.
-     * @param N           the number of elements expected.
-     * @param config      the configuration.
-     */
-    protected InsertionSort(String description, int N, Config config) {
-        super(description, N, config);
-    }
+	/**
+	 * Constructor for any sub-classes to use.
+	 *
+	 * @param description the description.
+	 * @param N           the number of elements expected.
+	 * @param config      the configuration.
+	 */
+	protected InsertionSort(String description, int N, Config config) {
+		super(description, N, config);
+	}
 
-    /**
-     * Constructor for InsertionSort
-     *
-     * @param N      the number elements we expect to sort.
-     * @param config the configuration.
-     */
-    public InsertionSort(int N, Config config) {
-        this(DESCRIPTION, N, config);
-    }
+	/**
+	 * Constructor for InsertionSort
+	 *
+	 * @param N      the number elements we expect to sort.
+	 * @param config the configuration.
+	 */
+	public InsertionSort(int N, Config config) {
+		this(DESCRIPTION, N, config);
+	}
 
-    public InsertionSort() {
-        this(new BaseHelper<>(DESCRIPTION));
-    }
+	public InsertionSort() {
+		this(new BaseHelper<>(DESCRIPTION));
+	}
 
-    /**
-     * Constructor for InsertionSort
-     *
-     * @param helper an explicit instance of Helper to be used.
-     */
-    public InsertionSort(Helper<X> helper) {
-        super(helper);
-    }
+	/**
+	 * Constructor for InsertionSort
+	 *
+	 * @param helper an explicit instance of Helper to be used.
+	 */
+	public InsertionSort(Helper<X> helper) {
+		super(helper);
+	}
 
-    /**
-     * Sort the sub-array xs:from:to using insertion sort.
-     *
-     * @param xs   sort the array xs from "from" to "to".
-     * @param from the index of the first element to sort
-     * @param to   the index of the first element not to sort
-     */
-    public void sort(X[] xs, int from, int to) {
-        final Helper<X> helper = getHelper();
+	/**
+	 * Sort the sub-array xs:from:to using insertion sort.
+	 *
+	 * @param xs   sort the array xs from "from" to "to".
+	 * @param from the index of the first element to sort
+	 * @param to   the index of the first element not to sort
+	 */
+	public void sort(X[] xs, int from, int to) {
+		final Helper<X> helper = getHelper();
 
-        // TO BE IMPLEMENTED
-    }
+		// TO BE IMPLEMENTED
+		for (int i = from + 1; i < to; i++) {
+			for (int j = i; j >= from + 1; j--) {
+				
+				if (helper.less(xs[j], xs[j-1])) {
+					helper.swap(xs, j - 1, j);
+				} else {
+					break;
+				}
+			}
+		}
+	}
 
-    /**
-     * This is used by unit tests.
-     *
-     * @param ys  the array to be sorted.
-     * @param <Y> the underlying element type.
-     */
-    public static <Y extends Comparable<Y>> void mutatingInsertionSort(Y[] ys) {
-        new InsertionSort<Y>().mutatingSort(ys);
-    }
 
-    public static final String DESCRIPTION = "Insertion sort";
+	/**
+	 * This is used by unit tests.
+	 *
+	 * @param ys  the array to be sorted.
+	 * @param <Y> the underlying element type.
+	 */
+	public static <Y extends Comparable<Y>> void mutatingInsertionSort(Y[] ys) {
+		new InsertionSort<Y>().mutatingSort(ys);
+	}
+
+	public static final String DESCRIPTION = "Insertion sort";
 
 }
